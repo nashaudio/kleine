cd4172d9b855de814af1139149d93ffc27d0db44
Chris Nash <chris.nash@uwe.ac.uk>
2026-04-03 20:37:13 +0100

    Comment out main function code in kleine.cpp

--- kleine.cpp
+++ kleine.cpp
@@ -13,23 +13,23 @@
 
 int main(int argc, char** argv)
 {
-	// create and configure the audio, synth, effect
-	Engine engine;
-	engine.start();
-	engine.attach<DX7>(0);
-    engine.attach<PingPong>(0.167, 0.75, 0.333, 0.25);
+	// // create and configure the audio, synth, effect
+	// Engine engine;
+	// engine.start();
+	// engine.attach<DX7>(0);
+ //    engine.attach<PingPong>(0.167, 0.75, 0.333, 0.25);
 
-	// play some notes
-	int notes[] = { 62, 64, 60, 48, 55 };
-	for(int note : notes) {
-		engine.noteOn(note, 127);
-		engine.wait(500);
-		engine.noteOff(note);
-	}
+	// // play some notes
+	// int notes[] = { 62, 64, 60, 48, 55 };
+	// for(int note : notes) {
+	// 	engine.noteOn(note, 127);
+	// 	engine.wait(500);
+	// 	engine.noteOff(note);
+	// }
 
-	// wait for notes to finish, stop the engine
-	engine.wait(1000);
-	engine.stop();
+	// // wait for notes to finish, stop the engine
+	// engine.wait(1000);
+	// engine.stop();
 	
     return 0;
 }
